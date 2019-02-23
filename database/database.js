0bfafa4bea40984b60b0d6e2951f850e6dd7b612
Omer92 <nahum.omer@gmail.com>
2019-02-23 17:02:49 +0200

    changed to postgres

--- database/database.js
+++ database/database.js
@@ -1,12 +1,10 @@
-var mysql = require('mysql');
+var { Pool } = require('pg');
 var util  = require('util');
-var pool = mysql.createPool({
-  connectionLimit: 15,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-})
+
+const connectionString = process.env.DATABASE_URL;
+var pool = new Pool({
+  connectionString: connectionString,
+});
 
 pool.getConnection((err, connection) => {
   if (err) {
