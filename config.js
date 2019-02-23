bde70870825acca3de8366d16b33bf6c0630b109
Omer92 <nahum.omer@gmail.com>
2019-02-23 19:39:25 +0200

    changed DB

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  DATABASE_URL: process.env.DATABASE_URL,
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
   DB_PASS: process.env.DB_PASS,
--- database/database.js
+++ database/database.js
@@ -1,11 +1,31 @@
-var { Pool } = require('pg');
+var mysql = require('mysql');
 var util  = require('util');
 
-const connectionString = process.env.DATABASE_URL;
-var pool = new Pool({
-  connectionString: connectionString,
-});
+var pool = mysql.createPool({
+  connectionLimit: 15,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+})
 
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.')
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.')
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.')
+    }
+  }
+  if (connection) {
+    connection.release()
+  }
+})
 
 pool.query = util.promisify(pool.query);
 
